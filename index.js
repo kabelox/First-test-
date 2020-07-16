06d6cbf7dda934346abd95c597bfe71035a102ac
Owethu99Twala <owethutwala99@gmail.com>
2020-07-16 11:39:12 +0200

    js funtion passing difference

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+function theDifference(n){
+    if(n >= 20){
+        return 20 -n;
+    }
+    else{
+        return (20 - n) * 0.5;
+    }
+}
+
+
+
+
+
+
+
+console.log(theDifference(10))
+
+
